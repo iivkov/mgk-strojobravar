7e96437e1992a368a7305ba1515f162bba2db5f5
Ivan <ivan.ivkovic1996@gmail.com>
2024-02-17 11:54:52 +0100

    nove izmjene po zahtjevu, novi build

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -12,22 +12,11 @@ const ProjectsComponent = () => {
         <h1>NOVI RAZVOJ U PETRINJI MGK STROJOBRAVAR OBRT</h1>
         <h2>Kod projekta: KK.08.2.1.16.0090</h2>
         <h2>Naziv poziva: Podrška razvoju poduzetništva u gradu Petrinji</h2>
-        <h2>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</h2>
-        <h2>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</h2>
         <div className="container">
             <img src={Petrinja} alt="Slika Petrinje." style={{ width: '100%', height: 'auto' }} />
             <p>Cilj ovog projekta je održiva fizička, socijalna i gospodarska regeneracija grada Petrinje, s ciljem smanjenja socijalnih nejednakosti, isključenosti i siromaštva. Nabavom novih roba i/ili usluga, Prijavitelj će povećati svoju: konkurentnost i održivost poslovanja i proizvodne kapacitete. Prijavitelj će otvoriti nova tržišta i povećat će mu se prihod. Projekt je usmjeren na djelatnike Prijavitelja, kupce i dobavljače kao ciljne skupine projekta, ali i sve građane Republike Hrvatske i to upravo kroz očuvanje postojećih radnih mjesta i otvaranje novih. Projekt vodi računa o zaštiti okoliša.</p>
         </div>
         <div className="container">
-            <div className="box-EU">
-                <h3>Podatci o korisniku</h3>
-                <p>Prijavitelj: MGK STROJOBRAVAR, OBRT ZA PROIZVODNJU I MONTAŽU</p>
-                <p>Jedinstveni broj prijavitelja: 90826361</p>
-                <p>Adresa: Trnjanska cesta 89, Zagreb</p>
-                <p>E-pošta: mgk.strojobravar@gmail.com</p>
-                <p>Telefon: +385 989068432</p>
-                <p>Odgovorna osoba: STIPAN SKOČIBUŠIĆ</p>
-            </div>
             <div className="box-EU">
                 <h3>Vrijeme provedbe</h3>
                 <p>Početak provedbe: 19.04.2022.</p>
@@ -38,43 +27,71 @@ const ProjectsComponent = () => {
             <div className="box-EU">
                 <h3>Informacije o troškovima</h3>
                 <p>Ukupni prihvatljivi troškovi (EUR): 210.554,78</p>
-                <p>Ukupna bespovratna sredstva (EUR): 178.971,56 (EU sufinanciranje)</p>
+                <p>Ukupna bespovratna sredstva (EUR): 178.971,56</p>
             </div>
         </div>
         <EUnoteProject />
         <div className="container">
             <div className="activity-box">
-                    <h3>Aktivnosti projekta</h3>
-                    <p>Aktivnost 1: Ulaganje u materijalnu imovinu (1/2)</p>
-                    <p>Aktivnost 2: Ulaganje u materijalnu imovinu (2/2)</p>
-                    <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
-                    <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
-                    <p>Aktivnost 5: Prijava na Poziv</p>
-                    <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
-                    <p>Aktivnost 7: Promicanje horizontalnih načela</p>
-                    <p>Aktivnost 8: Upravljanje projektom</p>
-                    <p>Aktivnost 9: Promidžba i vidljivost</p>
+                <h3>Aktivnosti projekta</h3>
+                <p>Aktivnost 1: Ulaganje u materijalnu imovinu (1/2)</p>
+                <p>Aktivnost 2: Ulaganje u materijalnu imovinu (2/2)</p>
+                <p>Aktivnost 3: Marketing i promocija proizvoda i usluga</p>
+                <p>Aktivnost 4: Edukacije, stručno osposobljavanje i stjecanje novih znanja zaposlenika</p>
+                <p>Aktivnost 5: Prijava na Poziv</p>
+                <p>Aktivnost 6: Provedba postupka nabave za potrebe projekta</p>
+                <p>Aktivnost 7: Promicanje horizontalnih načela</p>
+                <p>Aktivnost 8: Upravljanje projektom</p>
+                <p>Aktivnost 9: Promidžba i vidljivost</p>
             </div>
         </div>
         <div className="container">
             <div className="activity-box">
-                    <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
-                    <p>1. planiran je postupak nabave</p>
-                    <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
-                    <p>3. proveden je postupak nabave</p>
-                    <p>4. odabran je isporučitelj roba i/ili usluga</p>
-                    <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima
-i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
-                    <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u
-Ugovora.</p>
-                    <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
-                    <p>8. izdan je račun od strane isporučitelja</p>
-                    <p>9. račun je plaćen u roku</p>
-                    <br />
-                    <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
-                    <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
-                    <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
-                    <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+                <h3>Sve aktivnosti nabave iz predmetnog projekta</h3>
+                <p>1. planiran je postupak nabave</p>
+                <p>2. određene su karakteristike roba i/ili usluga koje se planiraju nabavljati u postupku nabave</p>
+                <p>3. proveden je postupak nabave</p>
+                <p>4. odabran je isporučitelj roba i/ili usluga</p>
+                <p>5. predmetna aktivnost je provedena u cijelosti, kako je bilo planirano, sukladno propisanim pravilima i projektnim prijedlogom (pridržavanje vremenskih rokova početka i završetka provedbe aktivnosti)</p>
+                <p>6. roba i/ili usluga je od strane odabranog isporučitelja isporučena na vrijeme, do roka određenog u Ugovoru.</p>
+                <p>7. zaključen je zapisnik o primopredaji roba i/ili usluga</p>
+                <p>8. izdan je račun od strane isporučitelja</p>
+                <p>9. račun je plaćen u roku</p>
+                <br />
+                <p>U provedbi predmetne aktivnosti nije bilo značajnih problema.</p>
+                <p>Ostvareni su rezultati koji su bili planirani - nabava roba i/ili usluga.</p>
+                <p>Održane su planirane edukacije djelatnika, u količini radnih sati koje je ponudio isporučitelj.</p>
+                <p>Nabavljena roba i/ili usluga se koristi u svakodnevnom poslovanju gospodarskog subjekta.</p>
+            </div>
+        </div>
+        <EUnoteProject />
+        <div className="container">
+            <div className="activity-box">
+                <h3>Podatci o projektu</h3>
+                <p>Kod projekta: KK.08.2.1.16.0090</p>
+                <p>Posredničko tijelo razine 1 (PT1): Ministarstvo regionalnoga razvoja i fondova Europske unije</p>
+                <p>Posredničko tijelo razine 2 (PT2): Središnja agencija za financiranje i ugovaranje programa i projekata Europske unije</p>
+                <p>Početak provedbe: 19.04.2022.</p>
+                <p>Kraj provedbe: 29.11.2023.</p>
+                <p>Trajanje provedbe (mjeseci): 20</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                <h3>Podatci o korisniku</h3>
+                <p>Prijavitelj: MGK STROJOBRAVAR, OBRT ZA PROIZVODNJU I MONTAŽU</p>
+                <p>Jedinstveni broj prijavitelja: 90826361</p>
+                <p>Adresa: Trnjanska cesta 89, Zagreb</p>
+                <p>E-pošta: mgk.strojobravar@gmail.com</p>
+                <p>Telefon: +385 989068432</p>
+                <p>Odgovorna osoba: STIPAN SKOČIBUŠIĆ</p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="activity-box">
+                <h3>Informacije o troškovima</h3>
+                <p>Ukupni prihvatljivi troškovi (EUR): 210.554,78</p>
+                <p>Ukupna bespovratna sredstva (EUR): 178.971,56 (EU sufinanciranje)</p>
             </div>
         </div>
         <EUnoteProject />
